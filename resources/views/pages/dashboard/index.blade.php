a25aab401e13f2e10aec1198f6adaf40e4ac81d6
Valeria Stamenova <stam0012@hz.nl>
2023-11-10 12:22:26 +0100

    Changes on dashboard page

--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -1,7 +1,7 @@
 <?php
 
 use function Laravel\Folio\{middleware, name};
-//use function Livewire\Volt\{state};
+use function Livewire\Volt\{state, mount};
 
 name('dashboard');
 middleware(['auth', 'verified']);
@@ -9,21 +9,29 @@ middleware(['auth', 'verified']);
 ?>
 
 <x-layouts.app>
-    <x-slot name="header">
-        <h2 class="text-lg font-semibold leading-tight text-gray-800 dark:text-gray-200">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
 
     @volt('dashboard')
-        <div class="h-full py-12">
-            <div class="h-full mx-auto max-w-7xl sm:px-6 lg:px-8">
+    <div class="h-full py-12">
+        <div class="h-full mx-auto max-w-7xl sm:px-6 lg:px-8">
 
-                <div class="relative min-h-[500px] w-full h-full">
-                    <x-ui.placeholder />
+            <div class="grid grid-cols-3 relative w-full h-full">
+                <div class="border border-gray-500 rounded py-20">
+                    <div class="pl-5">
+                        <h2 class="text-2xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
+                            {{ __('Hi, ') . Auth::user()->name }}
+                        </h2>
+                        <h2 class="text-lg font-semibold leading-tight text-gray-800 dark:text-gray-200">
+                            {{ __('See where you left it off last time') }}
+                        </h2>
+                    </div>
+                    <img src="img/waving-penguin.png">
                 </div>
-
+                <div>
+                    Under construction :)
+                </div>
             </div>
+
         </div>
+    </div>
     @endvolt
 </x-layouts.app>
